f700a2f978f2f6c4c504cec807a07b779804ebbf
Zsolt Szakal <60626421+zsoltszakal@users.noreply.github.com>
2021-05-26 14:11:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,10 @@ string = input("Please enter string:\n")
 morse_code = []
 
 for char in string:
-    new_char = morse_dict[char.lower()]
-    morse_code.append(new_char)
+    if char == " ":
+        morse_code.append(" ")
+    else:
+        new_char = morse_dict[char.lower()]
+        morse_code.append(new_char)
 
-print(morse)
+print(morse_code)
